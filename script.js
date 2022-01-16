7b4ead3abea1b0034a7b19ec2061816ef8ad92c5
Kostya Polishchuk <ksemper.idem@gmail.com>
2022-01-16 23:01:57 +0100

    #5

--- script.js
+++ script.js
@@ -229,7 +229,8 @@ imgTargets.forEach(img => imgObserver.observe(img));
 //Slider
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
-const btnLeft = document.querySelector('.slider__btn--right');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
 
 let curSlide = 0;
 const maxSlide = slides.length;
@@ -237,6 +238,13 @@ const maxSlide = slides.length;
 // const slider = document.querySelector('.slider');
 // slider.style.transform = 'scale(0.4) translateX(-300px)';
 // slider.style.overflow = 'visible';
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`)
+  })
+}
+createDots();
 
 const goToSlide = function (slide) {
   slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`));
@@ -266,6 +274,11 @@ const prevSlide = function () {
 btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', prevSlide);
 
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.key === 'ArrowLeft') prevSlide();
+  e.key === 'ArrowRight' && nextSlide();
+})
 
 //////////////////////////////////////////////
 //////////////////////////////////////////////
